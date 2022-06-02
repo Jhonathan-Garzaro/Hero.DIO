d681a0b6a89e5360657f08e7b3acc377325924bc
Jhonathan Garzaro Lopes <104940362+Jhonathan-Garzaro@users.noreply.github.com>
2022-06-02 18:30:27 -0300

    Add files via upload

---
+++ estrutura basica/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using estrutura_basica.src;
+
+namespace MyApp
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+             Hero Arus = new Hero("Arus", 12, "Guerreiro");
+             
+           
+
+                
+                 Console.Write(Arus);
+                
+                 
+
+                {
+                    
+                }
+         }
+    }
+}
